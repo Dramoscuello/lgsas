f826e553d534da0fb187b61555e395bcd6ef7da6
Deimer Ramos Cuello <dramos.cuello@gmail.com>
2015-10-13 01:56:53 -0500

    nueva version de lg sas

---
+++ js/funciones.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('#cd-main-nav ul li a').on('click', function(e){
+		e.preventDefault();
+		var strAncla = $(this).attr('href');
+		$('body, html').stop('true, true').animate({
+			scrollTop: $(strAncla).offset().top
+		},1500);
+	});
+});
+$('.goTop').click(function(){
+	$('body, html').animate({
+		scrollTop: '0px'
+	});
+});
